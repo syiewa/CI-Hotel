6db3f9c3f81cf6e3780bb8c06648e7080c6934db
Syiewa <Syiewa@AsusDWC-PC>
2014-01-30 19:29:17 +0700

    Merge origin/master
    Conflicts:
    	application/models/m_user.php

--- application/models/m_order.php
+++ application/models/m_order.php
@@ -15,13 +15,13 @@ class M_order extends MY_Model {
     private $tabel_cc = 'cc_detail';
     private $tabel_user = 'guest';
     var $status = array(
-        0 => 'In Process',
-        1 => 'Approved',
-        2 => 'Order Sent',
-        3 => 'Cancelled',
-        4 => 'Waiting Approval'
+        0 => '<span class="label label-info">In Process</span>',
+        1 => '<span class="label label-primary">Approved</span>',
+        2 => '<span class="label label-success">Finish</span>',
+        3 => '<span class="label label-danger">Cancelled</span>',
+        4 => '<span class="label label-warning">Waiting Approval</span>'
     );
-        var $paymentMethods = array(
+    var $paymentMethods = array(
         1 => 'Credit Card'
     );
 
@@ -30,7 +30,31 @@ class M_order extends MY_Model {
         parent::__construct();
         parent::set_tabel('order', 'order_id');
     }
-    
+
+    public function get_recents($offset = 0, $limit = 5) {
+        $offset = (int) $offset;
+        $limit = (int) $limit;
+        $q = $this->db->query('SELECT 
+        `order_id`, 
+        g.nama_depan AS first_name, 
+        g.nama_belakang AS last_name, 
+        c.title AS class_title, 
+        g.cc_type,
+        `tgl_order`,
+        `payment_id`, 
+        `payment_total`, 
+        `order_status`, 
+        `check_in`, 
+        `check_out`
+        FROM `order` o 
+        LEFT JOIN ( 
+            (SELECT idclass,title FROM `class`) AS c,
+            (SELECT id,nama_depan,nama_belakang,cc_detail.* FROM guest
+            LEFT JOIN `cc_detail` ON guest.id = cc_detail.`cc_userid` GROUP BY guest.`id`
+            ) AS g )
+	ON (c.`idclass`=o.`class_id` AND g.`id`=o.`guest_id`) LIMIT ' . $offset . ',' . $limit . ';');
+        return $q->result();
+    }
 
     public function insert_guest($data = array()) {
         $this->db->insert($this->tabel_user, $data);
--- application/views/admin/components/page_head.php
+++ application/views/admin/components/page_head.php
@@ -25,7 +25,7 @@
             </li>
             <li class="dropdown"><a href="arnosa.net" class="dropdown-toggle" data-toggle="dropdown">Orders & Users<b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url('index.php/admin/kelas/'); ?>">All Order</a></li>
+                    <li><a href="<?php echo base_url('index.php/admin/order/'); ?>">All Order</a></li>
                     <li class="divider"></li>
                     <li><a href="<?php echo site_url('admin/users'); ?>">All Users</a></li>
                 </ul>
