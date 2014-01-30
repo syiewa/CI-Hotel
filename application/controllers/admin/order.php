b8a7e5c955791c4b8178c3dc0361696641a55412
Syiewa <Syiewa@AsusDWC-PC>
2014-01-30 19:36:10 +0700

    Merge origin/master

--- application/controllers/admin/order.php
+++ application/controllers/admin/order.php
@@ -42,8 +42,75 @@ class Order extends Admin_Controller {
         $this->data['pagination'] = $this->pagination->create_links();
         $this->data['content'] = 'admin/orders/index';
         $this->load->view($this->template, $this->data);
+//        $this->load->library('Jquery_pagination');
+//        $config['base_url'] = site_url('admin/order/ajax_page');
+//        /* Here i am indicating to the url from where the pagination links and the table section will be fetched */
+//        $config['full_tag_open'] = '<ul class="pagination">';
+//        $config['full_tag_close'] = '</ul>';
+//        $config['last_tag_close'] = '</li>';
+//        $config['next_tag_open'] = '<li>';
+//        $config['next_tag_close'] = '</li>';
+//        $config['prev_tag_open'] = '<li>';
+//        $config['prev_tag_close'] = '</li>';
+//        $config['cur_tag_open'] = '<li><a href="#">';
+//        $config['cur_tag_close'] = '</a></li>';
+//        $config['num_tag_open'] = '<li>';
+//        $config['num_tag_close'] = '</li>';
+//        $config['div'] = '#display-content';
+//        /* CSS selector  for the AJAX content */
+//
+//        $config['total_rows'] = count($this->m_order->get_all());
+//        $config['per_page'] = 1;
+//        $this->jquery_pagination->initialize($config);
     }
 
+    public function index() {
+
+//        $this->data['status'] = $this->m_order->status;
+//        $this->data['orders'] = $this->m_order->get_recents($offset, 1);
+//        $this->data['pagination'] = $this->jquery_pagination->create_links();
+//        $this->data['content'] = 'admin/orders/index';
+//        $this->load->view($this->template, $this->data);
+        $this->data['status'] = $this->m_order->status;
+        $this->data['orders'] = $this->m_order->get_all();
+        $this->data['content'] = 'admin/orders/index';
+        $this->load->view($this->template, $this->data);
+    }
+
+    public function get_ajax($offset = 0) {
+        $count = $this->m_order->get_all();
+        $perpage = 5;
+        if (count($count) > $perpage) {
+            $this->load->library('pagination');
+            $config['base_url'] = site_url('admin/order/get_ajax');
+            $config['total_rows'] = count($count);
+            $config['per_page'] = $perpage;
+            $config['uri_segment'] = 4;
+            $q = $this->pagination->initialize($config);
+            $offset = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+        } else {
+            $this->data['pagination'] = '';
+            $offset = 0;
+        }
+        $this->data['status'] = $this->m_order->status;
+        $this->data['orders'] = $this->m_order->get_recents($offset, $perpage);
+        $this->data['content'] = 'admin/orders/index';
+        if ($this->input->post('ajax', FALSE)) {
+            $data = array(
+                'result' => $this->data["orders"],
+                'status' => $this->data['status'],
+                'pagination' => $this->pagination->create_links()
+            );
+            echo json_encode($data);
+        }
+    }
+
+//    public function ajax_page($offset = 0) {
+//        $this->data['status'] = $this->m_order->status;
+//        $this->data['orders'] = $this->m_order->get_recents($offset);
+//        $this->data['pagination'] = $this->jquery_pagination->create_links();
+//        $this->load->view('admin/orders/index', $this->data);
+//    }
 }
 
 ?>
--- application/views/admin/orders/index.php
+++ application/views/admin/orders/index.php
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/master
 <div id = 'display-content'>
 <div class="page-header">
     <h1>Data Order</h1>
@@ -71,3 +72,139 @@ echo $pagination
         $(this).removeData('bs.modal');
     });
 </script>
+=======
+<div class="page-header">
+    <h1>Data Order</h1>
+</div>
+<div id = 'result_table'>
+    <table class="table table-hover table-responsive" id="containerid">
+        <thead>
+            <tr class="success">
+                <th>ID Order</th>
+                <th>Order Name</th>    
+                <th>Type Class</th>
+                <th>Order Date</th>
+                <th>Status</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody id="data">
+            <?php if ($orders): ?>
+                <?php
+//                $i = 1;
+//                foreach ($orders as $p) :
+//                    
+                ?>
+<!--                <tr class="data">
+                            <td id="order_id"><?php // echo $p->order_id;    ?></td>
+                    <td id="order_name"><?php // echo $p->first_name . ' ' . $p->last_name;    ?></td>
+                    <td id="class_title"><?php // echo $p->class_title;    ?></td>
+                    <td id="tgl_order"><?php // echo $p->tgl_order;    ?></td>
+                    <td id="order_status"><?php // echo $status[$p->order_status];    ?></td>
+                    <td>
+                        <a href="<?php // echo base_url('index.php/admin/orders/edit/' . $p->order_id);  ?>" class="btn btn-default btn-xs btn-primary" data-target="#telo" role="button" data-toggle="modal"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+                        <a href="<?php // echo base_url('index.php/admin/orders/gambar/' . $p->order_id);  ?>" class="btn btn-default btn-xs btn-primary" ><span class="glyphicon glyphicon-edit"></span> Galery</a>
+                        <?php // echo btn_delete('admin/orders/delete/' . $p->order_id); ?>
+                    </td>
+                </tr>-->
+                <?php
+//                    $i++;
+//                endforeach;
+//                
+                ?>
+            <?php else: ?>
+                <tr><td>Belum ada data !</td></tr> 
+            <?php endif; ?>
+        </tbody>
+    </table>
+    <!-- Modal -->
+
+    <div class="modal fade" id="telo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Event</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Loading...</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+
+            </div>
+            <!-- /.modal-content -->
+        </div>
+    </div><!-- /.modal-dialog -->
+    <div id="paging">
+    </div>
+</div>
+<script>
+    var base_url = "<?php echo site_url('admin/order'); ?>";</script>
+<script>
+    $('#telo').on('hidden.bs.modal', function() {
+        $(this).removeData('bs.modal');
+    });</script>
+<script>
+    $(document).ready(function() {
+        var source = $("#result_table").html();
+        if (source) {
+            function load_result(index) {
+                index = index || 0;
+                $.ajax({
+                    type: "POST",
+                    dataType: "JSON",
+                    url: base_url + '/get_ajax/' + index, //"/index/" + index,
+                    data: {ajax: true},
+                    success: function(returnedData) {
+                        //returnedData will now be an array Object
+                        var data = returnedData['result'];
+                        var html = '';
+                        for (var i = 0, len = data.length; i < len; ++i) {
+                            html += '<tr>';
+                            html += '<td>' + data[i].order_id + '</td>';
+                            html += '<td>' + data[i].first_name + ' ' + data[i].last_name + '</td>';
+                            html += '<td>' + data[i].class_title + '</td>';
+                            html += '<td>' + data[i].tgl_order + '</td>';
+                            html += '<td>' + returnedData['status'][data[i].order_status] + '</td>';
+                            html += "</tr>";
+                        }
+                        console.log(html);
+                        $('#data').html(html);
+                        $('#paging').html(returnedData['pagination']);
+//                        $('#order_id').html(returnedData['result'][0].order_id);
+//                        $('#order_name').html(returnedData['result'][0].first_name + ' ' + returnedData['result'][0].last_name);
+//                        $('#class_title').html(returnedData['result'][0].class_title);
+//                        $('#tgl_order').html(returnedData['result'][0].tgl_order);
+//                        $('#order_status').html(returnedData['status'][returnedData['result'][0].order_status]);
+                    },
+                    error: function() {
+                        alert("failure");
+                        $("#result").html('error while submitting');
+                    }
+                });
+//                $.post(base_url + "/index/" + index, {ajax: true}, function(data) {
+//                    $("#result_table").html(data.result);
+//                    // pagination
+//                    $('#pagination').html(data.pagination);
+//                }, "json");
+
+
+            }
+
+            //calling the function 
+            load_result();
+        }
+        //pagination update
+        $('#paging').on('click', '.pagination a', function(e) {
+            e.preventDefault();
+            //grab the last paramter from url
+            var link = $(this).attr("href").split(/\//g).pop();
+            load_result(link);
+            return false;
+        });
+    });
+</script>
+>>>>>>> 6db3f9c Merge origin/master
